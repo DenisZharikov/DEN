a9f91844bd35cdfe3ff99763c5b405dd0bdab4b9
den4ik <zharikovdenis0@gmail.com>
2023-07-04 20:48:05 +0300

    bla

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
+import java.net.SocketOption;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("DENDENDEN");
+        int a = 7; //16-9=7
+        int b = 9; //7
+        a = a+b;
+        b = a-b;
+        a = a-b;
+        System.out.println(a);
+        System.out.println(b);
+
     }
-        int a = 7;
-        int b = 9;
-vgf
 }
